7caee88cd22e1d8c2a50c0f4a547c99cf98ffe84
1ruf <gyeol0411@naver.com>
2025-01-25 04:01:45 +0900

    .update StatusSystem

--- Assets/2.Script/Player/StatusManager.cs
+++ Assets/2.Script/Player/StatusManager.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public class StatusManager : MonoBehaviour
 {
-    public float _stamina { get; private set; } = 10f;
+    public float Stemina { get; private set; } = 10f;
+    public float Health { get; private set; } = 100f;
 }
